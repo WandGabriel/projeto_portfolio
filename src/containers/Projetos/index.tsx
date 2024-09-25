0fcae5d48b4f07d68e22efc3d41eecc0273de777
Wanderson Gabriel <gabriel_wanderson@hotmail.com>
2024-09-25 19:24:26 -0300

    Criando a seção de projetos e o componente de projetos.

--- src/containers/Projetos/index.tsx
+++ src/containers/Projetos/index.tsx
@@ -1,5 +1,38 @@
+import Projeto from '../../components/Projeto'
 import Titulo from '../../components/Titulo'
 
-const Projetos = () => <Titulo fontSize={16}>Projetos</Titulo>
+import { Lista } from './styles'
+
+const Projetos = () => (
+  <section>
+    <Titulo fontSize={16}>Projetos</Titulo>
+    <Lista>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+    </Lista>
+  </section>
+)
 
 export default Projetos
